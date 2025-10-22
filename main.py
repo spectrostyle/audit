f06ed30915ed2bb3056a988c97ec7431580bd76c
greg <spectrosyle@gmail.com>
2025-10-21 22:31:41 -0400

    hotel statistics is working, pulling needed values

--- main.py
+++ main.py
@@ -13,14 +13,15 @@ def main():
 
     found_files = f"{os.getcwd()}/FoundFiles"
     reports_file = f"{os.getcwd()}/AuditFiles"
+    template = f"{os.getcwd()}/Template/SummaryTemplate.HTML"
 
 
     startup.startup()
     fileOperations.clean(found_files)
     fileOperations.find_move(reports_file, found_files)
 
-    hotel_Stats = hotelStatistics.one(f"{found_files}/HotelStatistics.csv")
-
+    hotel_stats = hotelStatistics.one(f"{found_files}/HotelStatistics.csv")
+    print(hotel_stats)
 
 
 
